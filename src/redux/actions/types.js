564cf4d4f01be0c276061c263cf92c69d75dccaf
hirom <hirom0808@gmail.com>
2022-04-07 08:55:47 +0900

    Calculator完成！

---
+++ src/redux/actions/types.js
@@ -0,0 +1,14 @@
+// 「数字」がクリックされた時
+export const INPUT_NUMBER = "INPUT_NUMBER";
+
+// [+-*/]がクリックされた時
+export const PLUS = "PLUS";
+export const MINUS = "MINUS";
+export const MULTIPLY = "MULTIPLY";
+export const DIVIDE = "DIVIDE";
+
+// イコールがクリックされた時
+export const EQUAL = "EQUAL";
+
+// ACがクリックされたとき
+export const CLEAR = "CLEAR";
